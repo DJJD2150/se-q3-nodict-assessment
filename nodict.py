3a5aa5cbb60d7c5d6f718e2855efb4b5b437d4c7
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2020-07-23 14:21:10 -0400

    Initial commit to DJJD2150's Github.

--- nodict.py
+++ nodict.py
@@ -3,47 +3,68 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = "DJJD2150, Ybrayym Abamov"
 
 
 class Node:
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
-        return
+        """This dunder function defines the keys and values and hashes them."""
+        self.hash = hash(key)
+        self.key = key
+        self.value = value
 
     def __repr__(self):
-        # Your code here
-        return
+        """This puts them into readable string format."""
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        """This compares for potential duplicates."""
+        return self.key == other.key
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        """Creates a new instance of NoDict."""
+        self.buckets = []
+        self.size = num_buckets
+        for i in range(num_buckets):
+            self.buckets.append([])
 
     def __repr__(self):
-        # Your code here
-        return
+        """Return a string representing the NoDict contents."""
+        # We want to show all the buckets vertically
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}'
+                          for i, bucket in enumerate(self.buckets)])
 
     def add(self, key, value):
-        # Your code here
-        return
+        """Adds an additional key/ value pair to NoDict and
+        checks for potential duplicates."""
+        new_node = Node(key, value)
+        random_bucket = new_node.hash % self.size
+        for node in enumerate(self.buckets[random_bucket]):
+            if new_node == node[1]:
+                del self.buckets[random_bucket][node[0]]
+        self.buckets[random_bucket].append(new_node)
 
     def get(self, key):
-        # Your code here
-        return
+        """Gets a key/ value pair from NoDict and checks if it exists."""
+        value = None
+        node_to_find = Node(key)
+        random_bucket = node_to_find.hash % self.size
+        for node in self.buckets[random_bucket]:
+            if node_to_find == node:
+                value = node.value
+        if value is None:
+            raise KeyError(f'{key} not found')
+        return value
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        """Enables the square bracket reading behavior."""
+        value = self.get(key)
+        if value is None:
+            raise KeyError(f'{key} not found')
+        return value
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        """Enables the square bracket assignment behavior."""
+        return self.add(key, value)
